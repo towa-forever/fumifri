a01f34e07d96a63ad1fac1fd5d574799cefa905e
towa-forever <towa08062026@outlook.jp>
2026-06-17 23:25:39 +0900

    fix: syncIndexes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: '5mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
+  .then(async () => { console.log('MongoDB connected'); await User.syncIndexes(); })
   .catch(err => console.error(err));
 
 const userSchema = new mongoose.Schema({
